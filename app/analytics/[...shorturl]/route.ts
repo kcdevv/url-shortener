fe0da9766340bbf037a968662d99f2ff3a06f5fa
krisnachaitanya <stealgamerofficial@gmail.com>
2025-02-18 20:24:57 +0530

    updated analytics

--- app/analytics/[...shorturl]/route.ts
+++ app/analytics/[...shorturl]/route.ts
@@ -2,7 +2,8 @@ import { client } from "@/app/db";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest){
-    const shorturl = req.nextUrl.pathname.slice(1);
+    const shorturl = req.nextUrl.pathname.split("/analytics")[1].slice(1);
+    console.log(shorturl)
     const link = await client.link.findUnique({
         where: { id: shorturl }
     });
